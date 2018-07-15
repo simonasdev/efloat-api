5d2bc5c2fd70351cc05fb7587d04b19c29733998
Simonas Gildutis <simonasgildutis@gmail.com>
2018-07-15 23:14:46 +0300

    notify

--- app/models/device.rb
+++ app/models/device.rb
@@ -38,6 +38,7 @@ class Device < ApplicationRecord
   private
 
   def notify_changes
+    Rails.logger.info "notifying, #{saved_changes}, #{self}"
     $redis.publish CHANNEL, DeviceSerializer.new(self).to_json
   end
 end
