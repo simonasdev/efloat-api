6fbf7d7e9033266a3cae3e2a1ea1fdaf7e6ed30c
Simonas Gildutis <simonasgildutis@gmail.com>
2018-07-27 19:43:12 +0300

    clear interval

--- app/assets/javascripts/devices.js
+++ app/assets/javascripts/devices.js
@@ -1,9 +1,14 @@
 function initializeConnectedDevices () {
+  var interval;
+
   if ($('#connected-devices').length) {
-    window.setInterval(function () {
+    interval = window.setInterval(function () {
       $.get('/devices/connected', function (response) {
         $('#connected-devices').html(response);
       });
     }, 2000);
+  } else if (interval) {
+    window.clearInterval(interval);
+    interval = null;
   }
 }
