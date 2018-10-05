91053f86246fcd5e3af1781dd5ddc898529e4152
Simonas Gildutis <simonasgildutis@gmail.com>
2018-10-05 16:27:46 +0300

    race tracks by kind

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,6 +1,11 @@
 class RaceSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :title, :start_time, :end_time
-  has_many :tracks
+
+  has_many :tracks do |object, params|
+    object.tracks.where(kind: params[:kind])
+  end
+
   has_many :markers
+
 end
