ec04d09b354ccf91311c29357d7343c664ce4c3d
Simonas Gildutis <simonasgildutis@gmail.com>
2018-10-06 13:28:10 +0300

    remove obsolete javascript

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -2,62 +2,7 @@ window.addEventListener('message', handleMessage, false);
 
 $(document).on('change', '.btn-file input', function () {
   $(event.target).closest('form').submit();
-}).on('input', '#period', function (event) {
-  var target = event.target;
-  var $anchor = $(target).closest('.links').find('a').first();
-  var href = $anchor.attr('href');
-
-  $anchor.attr('href', href.split('=')[0] + '=start,' + target.value);
-}).on('click', '#speed-report', function (event) {
-  var $map = $('#map');
-  var speedThreshold = parseInt($('#speed').val());
-  var tracks = $map.data('tracks').filter(function (track) { return track.kind === 'limited'; });
-  var offenses = {};
-
-  var deviceIds = [2, 12, 22, 21, 26, 47, 85, 39, 48, 20, 49, 29, 38, 61, 75, 17, 57, 56, 53, 30, 71, 10, 58, 27, 52, 72, 3, 62, 1, 6, 43, 34, 70, 46, 42, 33, 8, 63, 25, 14, 50, 55, 45, 18, 37, 24, 9, 16, 15, 11, 40, 69, 51, 74, 5, 44, 67, 7, 66, 68, 13, 35, 19, 23, 54, 32, 60, 36]
-
-  deviceIds.forEach(function (id, i) {
-    $.get('/devices/' + id + '/data_lines', function (response) {
-      var data = response.data
-
-      tracks.forEach(function (track) {
-        var route = track.route.map(function (coordinate) {
-          var latitude = coordinate[0],
-              longitude = coordinate[1];
-
-          return new L.LatLng(latitude, longitude);
-        });
-
-        data.forEach(function (dataLine) {
-          var attrs = dataLine.attributes;
-
-          // if (attrs.speed > track.speed_limit + speedThreshold) {
-          //   var point = new L.LatLng(attrs.latitude, attrs.longitude);
-
-          //   if (isPointOnLine(point, route)) {
-          //     offenses[track.id] = offenses[track.id] || [];
-          //     offenses[track.id].push(dataLine.id);
-          //   }
-          // }
-          if (new Date(attrs.timestamp) < new Date(track.start_time) || new Date(attrs.timestamp) > new Date(track.end_time)) {
-            var point = new L.LatLng(attrs.latitude, attrs.longitude);
-
-            if (isPointOnLine(point, route)) {
-              offenses[track.id] = offenses[track.id] || [];
-              offenses[track.id].push(dataLine.id);
-            }
-          }
-        });
-      });
-
-      if (i === deviceIds.length - 1) {
-        console.log(JSON.stringify(offenses));
-      }
-    });
-  });
-
-  event.preventDefault();
-});
+})
 
 function handleMessage (event) {
   if (event.origin === window.allowedOrigin) {
