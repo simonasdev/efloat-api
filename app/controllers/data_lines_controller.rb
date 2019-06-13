0d5c288813326e51af4181a41aeea55ac0c9e8b4
Simonas Gildutis <simonasgildutis@gmail.com>
2019-06-13 18:35:53 +0300

    log log log

--- app/controllers/data_lines_controller.rb
+++ app/controllers/data_lines_controller.rb
@@ -4,7 +4,7 @@ class DataLinesController < ApplicationController
                   .where(params.permit(:device_id, :race_id))
                   .by_timestamp(*params.values_at(:timestamp_from, :timestamp_until))
                   .order(:timestamp)
-
+    p data_lines.to_sql
     render json: BasicDataLineSerializer.new(data_lines)
   end
 end
