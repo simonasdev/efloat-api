105f4e0f0ca3db96fc4684354f3cc8d704282d4d
Simonas Gildutis <simonasgildutis@gmail.com>
2019-06-13 18:45:41 +0300

    parse timestamps in time zone

--- app/controllers/data_lines_controller.rb
+++ app/controllers/data_lines_controller.rb
@@ -1,10 +1,12 @@
 class DataLinesController < ApplicationController
   def index
+    timestamps = params.values_at(:timestamp_from, :timestamp_until).map(&Time.zone.method(:parse))
+
     data_lines = DataLine
                   .where(params.permit(:device_id, :race_id))
-                  .by_timestamp(*params.values_at(:timestamp_from, :timestamp_until))
+                  .by_timestamp(*timestamps)
                   .order(:timestamp)
-    Rails.logger.info data_lines.to_sql
+
     render json: BasicDataLineSerializer.new(data_lines)
   end
 end
