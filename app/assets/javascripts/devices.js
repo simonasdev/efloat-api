104d48d3810cd89de6490d3ded49aded043ebf08
Simonas Gildutis <simonasgildutis@gmail.com>
2019-10-05 22:32:10 +0300

    rescue something???

--- app/assets/javascripts/devices.js
+++ app/assets/javascripts/devices.js
@@ -8,6 +8,15 @@ function initializeTimelineMap() {
         timeDimension: true
       })
     );
+    var redIcon = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 40],
+      iconAnchor: [12, 40],
+      popupAnchor: [1, -20],
+      tooltipAnchor: [12, -20],
+      shadowSize: [40, 40]
+    });
 
     var timeDimensionControl = new L.Control.TimeDimension({
       minSpeed: 1,
@@ -20,10 +29,70 @@ function initializeTimelineMap() {
 
     addTracksToMap($map.data('tracks'), map);
 
-    $('#refresh-data').on('click', replaceDataLinesForMap.bind(null, $map.data('url'), map));
+    var marker;
+
+    $('#refresh-data').on('click', replaceDataLines);
 
     if ($('#timestamp_from').val() && $('#timestamp_until').val()) {
       $('#refresh-data').click();
+    }
+
+    function replaceDataLines() {
+      var params = '&timestamp_from=' + $('#timestamp_from').val() + '&timestamp_until=' + $('#timestamp_until').val()
+
+      return $.get($map.data('url') + params, function (response) {
+        var coordinates = [],
+          times = [],
+          speeds = [],
+          attributes,
+          data = response.data;
+
+        for (var i = 0, len = data.length; i < len; i++) {
+          attributes = data[i].attributes;
+
+          coordinates.push([attributes.longitude, attributes.latitude]);
+          times.push(attributes.timestamp);
+          speeds.push(attributes.speed);
+        }
+
+        var layer = L.geoJSON({
+          type: 'Feature',
+          geometry: {
+            type: 'MultiPoint',
+            coordinates: coordinates
+          },
+          properties: {
+            times: times,
+            speed: speeds
+          }
+        }, {
+          pointToLayer: function (feature, latLng) {
+            if (marker) {
+              marker.setLatLng(latLng)
+            } else {
+              marker = L.marker(latLng, { icon: redIcon })
+            }
+
+            return marker;
+          },
+          onEachFeature: function (feature, layer) {
+            var speed = feature.properties.speed[
+              timeDimension && timeDimension._map.timeDimension._currentTimeIndex || 0
+            ];
+
+            if (speed) {
+              layer.unbindTooltip().bindTooltip(Math.round(speed) + ' km/h', { permanent: true });
+            }
+          }
+        });
+
+        var timeDimension = L.timeDimension.layer.geoJson(layer, {
+          updateTimeDimension: true,
+          updateTimeDimensionMode: 'replace',
+        });
+
+        timeDimension.addTo(map);
+      });
     }
   }
 }
@@ -39,70 +108,4 @@ function initializeConnectedDevices () {
     window.clearInterval(window.connectedDevicesInterval);
     window.connectedDevicesInterval = null;
   }
-}
-
-function replaceDataLinesForMap(url, map) {
-  var redIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 40],
-    iconAnchor: [12, 40],
-    popupAnchor: [1, -20],
-    tooltipAnchor: [12, -20],
-    shadowSize: [40, 40]
-  });
-
-  var params = '&timestamp_from=' + $('#timestamp_from').val() + '&timestamp_until=' + $('#timestamp_until').val()
-
-  return $.get(url + params, function (response) {
-    var coordinates = [],
-        times = [],
-        speeds = [],
-        attributes,
-        data = response.data;
-
-    for (var i = 0, len = data.length; i < len; i++) {
-      attributes = data[i].attributes;
-
-      coordinates.push([attributes.longitude, attributes.latitude]);
-      times.push(attributes.timestamp);
-      speeds.push(attributes.speed);
-    }
-
-    var marker;
-
-    var layer = L.geoJSON({
-      "type": "Feature",
-      "geometry": {
-        "type": "MultiPoint",
-        "coordinates": coordinates
-      },
-      "properties": {
-        "times": times,
-        speed: speeds
-      }
-    }, {
-      pointToLayer: function (feature, latLng) {
-        if (marker) {
-          marker.setLatLng(latLng)
-        } else {
-          marker = L.marker(latLng, { icon: redIcon })
-        }
-
-        return marker;
-      },
-      onEachFeature: function (feature, layer) {
-        var speed = feature.properties.speed[timeDimension && timeDimension._map.timeDimension._currentTimeIndex || 0];
-
-        layer.bindTooltip(Math.round(speed) + ' km/h', { permanent: true });
-      }
-    });
-
-    var timeDimension = L.timeDimension.layer.geoJson(layer, {
-      updateTimeDimension: true,
-      updateTimeDimensionMode: 'replace',
-    });
-
-    timeDimension.addTo(map);
-  });
 }
